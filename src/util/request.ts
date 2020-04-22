8c96ccc2497e6b4cf4ab1fda8e7d728cb4a19eae
el'diablo <eldiablolives@live.com>
2020-04-22 05:14:04 +0000

    request util

--- src/util/request.ts
+++ src/util/request.ts
@@ -1,81 +1,95 @@
-import { ApplicationError } from '../error';
+const agent = require('superagent');
 
-module.exports.get = async (url: string, query: any) => {
-  return new Promise((resolve, reject) => {
-    const request = require('request');
-    request.get(
-      {
-        url: url,
-        qs: query,
-        json: true
-      },
-      (err, httpResponse, body) => {
-        if (httpResponse.statusCode !== 200) {
-          return reject(new ApplicationError(body.error, body.error_description, `remote_${body.trace}`));
-        }
-        return resolve(body);
-      }
-    );
-  });
+module.exports.post = async function (url: string, data: any, token?: string): Promise<any> {
+  return req('POST', url, data, token, true);
 };
 
-module.exports.post = async (url: string, form: any) => {
-  return new Promise((resolve, reject) => {
-    const request = require('request');
-    request.post(
-      {
-        url: url,
-        form: form,
-        json: true
-      },
-      (err, httpResponse, body) => {
-        if (httpResponse.statusCode !== 200) {
-          return reject(new ApplicationError(body.error, body.error_description, `remote_${body.trace}`));
-        }
-        return resolve(body);
-      }
-    );
-  });
+module.exports.put = async function (url: string, data: any, token?: string): Promise<any> {
+  return req('PUT', url, data, token, true);
 };
 
-module.exports.put = async (url: string, form: any) => {
-  return new Promise((resolve, reject) => {
-    const request = require('request');
-    request.put(
-      {
-        url: url,
-        form: form,
-        json: true
-      },
-      (err, httpResponse, body) => {
-        if (httpResponse.statusCode !== 200) {
-          return reject(new ApplicationError(body.error, body.error_description, `remote_${body.trace}`));
-        }
-        return resolve(body);
-      }
-    );
-  });
+module.exports.patch = async function (url: string, data: any, token?: string): Promise<any> {
+  return req('PATCH', url, data, token, true);
 };
 
-module.exports.del = async (url: string, query: any) => {
-  return new Promise((resolve, reject) => {
-    const request = require('request');
-    request.del(
-      {
-        url: url,
-        qs: query,
-        json: true
-      },
-      (err, httpResponse, body) => {
-        if (httpResponse.statusCode !== 200) {
-          return reject(new ApplicationError(body.error, body.error_description, `remote_${body.trace}`));
-        }
-        return resolve(body);
-      }
-    );
-  });
+module.exports.get = async function (url: string, query: any, token?: string): Promise<any> {
+  return req('GET', url, query, token, true);
 };
 
-function sanitize(error: any) {
-  return error.error;
+module.exports.del = async function (url: string, query: any, token?: string): Promise<any> {
+  return req('DELETE', url, query, token, true);
+};
+
+async function req(method: string, url: string, data?: any, token?: string, raw?: boolean): Promise<any> {
+  method = method.toUpperCase();
+
+  const rq: any = {
+    method: method,
+    uri: url,
+    json: true,
+  };
+
+  if (data) {
+    if (method === 'GET' || method === 'DELETE') {
+      rq.qs = data;
+    } else {
+      if (raw) {
+        rq.body = data;
+      } else {
+        rq.form = data;
+      }
+    }
+  }
+
+  if (token) {
+    rq.headers = { Authorization: `Bearer ${token}` };
+  }
+
+  let params: any = {};
+  if (token) {
+    params['Authorization'] = `Bearer ${token}`;
+  }
+
+  if (!raw) {
+    params['accept'] = 'json';
+  }
+
+  let req;
+
+  return new Promise((resolve, reject) => {
+    switch (method) {
+      case 'GET':
+        req = agent.get(url).query(data);
+        break;
+      case 'DELETE':
+        req = agent.del(url).query(data);
+        break;
+      case 'POST':
+        req = agent.post(url).send(data);
+        break;
+      case 'PUT':
+        req = agent.put(url).send(data);
+        break;
+      case 'PATCH':
+        req = agent.patch(url).send(data);
+        break;
+    }
+
+    req.set(params);
+    req
+      .then((result) => {
+        if (result.status === 200) {
+          if (raw) {
+            return resolve(JSON.parse(result.text));
+          }
+          return resolve(result.text);
+        }
+      })
+      .catch((err) => {
+        if (raw) {
+          return reject(JSON.parse(err.response.text));
+        }
+        return reject(err.response.text);
+      });
+  });
 }
