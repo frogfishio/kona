fcbe1682bb541efedb01546740b12745a2d920e7
el'diablo <eldiablolives@live.com>
2021-06-24 16:17:45 +0700

    memory timeout after 60 sec

--- src/component/memory.ts
+++ src/component/memory.ts
@@ -1,7 +1,7 @@
-import { Engine } from '..';
-import { Component } from './index';
-import { connect } from 'tls';
-import { ApplicationError } from '../error';
+import { Engine } from "..";
+import { Component } from "./index";
+import { connect } from "tls";
+import { ApplicationError } from "../error";
 
 let logger;
 
@@ -11,84 +11,87 @@ export class Memory implements Component {
   private client;
   private _salt;
   private _cipher;
-  private _crypto = require('crypto');
+  private _crypto = require("crypto");
+  private _error_count = 0;
 
   constructor(private system: Engine) {
-    logger = system.log.log('engine:memory');
-    this.conf = system.configuration.get('memory');
-    this.id = system.configuration.get('system').id.toLowerCase();
-    this._salt = system.configuration.get('system').salt;
+    logger = system.log.log("engine:memory");
+    this.conf = system.configuration.get("memory");
+    this.id = system.configuration.get("system").id.toLowerCase();
+    this._salt = system.configuration.get("system").salt;
 
     this.conf.options = this.conf.options || {};
-    this.conf.options.retry_strategy = function(options) {
-      if (options.error && options.error.code === 'ECONNREFUSED') {
-        return new Error('Unable to connect to memory cluster');
+    this.conf.options.retry_strategy = function (options) {
+      if (options.error && options.error.code === "ECONNREFUSED") {
+        return new Error("Unable to connect to memory cluster");
       }
     };
   }
 
   init(): Promise<any> {
     return new Promise((resolve, reject) => {
-      var Redis = require('ioredis');
-      logger.info(
-        `Connecting to memory cluster ${this.conf.name} using ${this.conf.model} model`
-      );
+      var Redis = require("ioredis");
+      logger.info(`Connecting to memory cluster ${this.conf.name} using ${this.conf.model} model`);
 
       switch (this.conf.model) {
-        case 'sentinel':
+        case "sentinel":
           this.client = new Redis({
             sentinels: this.conf.hosts,
             name: this.conf.name,
-            password: this.conf.password
+            password: this.conf.password,
           });
-          this.client.on('error', err => {
+          this.client.on("error", (err) => {
+            this._error_count++;
+            if (this._error_count >= 60) {
+              return reject("Error connecting to memory cluster");
+            }
             logger.warn(err.message);
           });
-          this.client.on('connect', () => {
-            logger.info('Memory back-end connected');
+          this.client.on("connect", () => {
+            this._error_count = 0;
+            logger.info("Memory back-end connected");
             return resolve(null);
           });
           break;
-        case 'direct':
+        case "direct":
           this.client = new Redis({
             host: this.conf.host,
             port: this.conf.port,
-            password: this.conf.password
+            password: this.conf.password,
           });
-          this.client.on('error', err => {
+          this.client.on("error", (err) => {
+            this._error_count++;
+            if (this._error_count >= 60) {
+              return reject("Error connecting to memory cluster");
+            }
             logger.warn(err.message);
           });
-          this.client.on('connect', () => {
-            logger.info('Memory back-end connected');
+          this.client.on("connect", () => {
+            this._error_count = 0;
+            logger.info("Memory back-end connected");
             return resolve(null);
           });
           break;
         default:
-          return reject('Unsupported memory connector model');
+          return reject("Unsupported memory connector model");
       }
     });
   }
 
   release(): Promise<any> {
     this.client.quit();
-    logger.info('Released');
+    logger.info("Released");
     return Promise.resolve(null);
   }
 
   async get(key: string): Promise<any> {
-    const rkey = this.id + '-' + key;
+    const rkey = this.id + "-" + key;
 
     return new Promise((resolve, reject) => {
       this.client.get(rkey, (err, data) => {
         if (err) {
           logger.error(err);
-          return reject(
-            new ApplicationError(
-              'system_error',
-              'Error getting data from memory store',
-              'sys_mem_get1'
-            )
-          );
+          return reject(new ApplicationError("system_error", "Error getting data from memory store", "sys_mem_get1"));
         }
 
         if (!data) {
@@ -106,10 +109,10 @@ export class Memory implements Component {
   }
 
   async set(key: string, value: any, ttl?: number) {
-    const rkey = this.id + '-' + key;
+    const rkey = this.id + "-" + key;
     let data;
 
-    if (typeof value === 'object') {
+    if (typeof value === "object") {
       data = JSON.stringify(value);
     } else {
       data = JSON.stringify({ _payload: value });
@@ -127,20 +130,20 @@ export class Memory implements Component {
   }
 
   async remove(key: string) {
-    this.client.del(this.id + '-' + key);
+    this.client.del(this.id + "-" + key);
   }
 
   private encrypt(secret, data) {
-    const cipher = this._crypto.createCipher('aes-192-cbc', secret);
-    data = cipher.update(data, 'utf8', 'hex');
-    data += cipher.final('hex');
+    const cipher = this._crypto.createCipher("aes-192-cbc", secret);
+    data = cipher.update(data, "utf8", "hex");
+    data += cipher.final("hex");
     return data;
   }
 
   private decrypt(secret, data) {
-    const decipher = this._crypto.createDecipher('aes-192-cbc', secret);
-    data = decipher.update(data, 'hex', 'utf8');
-    data += decipher.final('utf8');
+    const decipher = this._crypto.createDecipher("aes-192-cbc", secret);
+    data = decipher.update(data, "hex", "utf8");
+    data += decipher.final("utf8");
     return data;
   }
 }
