8daa66fb71c6b1dd2c42068506e151139c837094
el'diablo <eldiablolives@live.com>
2021-07-02 09:04:27 +0700

    test fixes

--- test/file.ts
+++ test/file.ts
@@ -26,7 +26,6 @@ describe('File', () => {
       ).access_token;
   });
 
-  it('should get list of files as a ping');
   it('should upload file', async () => {
     const rs = fs.createReadStream('/home/eldiablo/Work/Kona/core/kona/test/test.jpg');
     testFile = await engine.files.create(`test_file_${TIME}.jpg`, rs, null, { hello: 'world' });
@@ -41,9 +40,9 @@ describe('File', () => {
   });
 
   it('should find file by ID', async () => {
-    expect(await engine.files.find({ _uuid: testFile.id }))
-      .to.be.instanceof(Array)
-      .with.length(1);
+    const res = await engine.files.find({ _uuid: testFile.id });
+    expect(res).to.be.instanceof(Array).with.length(1);
+    expect(res[0]).to.have.property('_uuid').which.equals(testFile.id);
   });
 
   it('should get file payload', async () => {
@@ -66,5 +65,16 @@ describe('File', () => {
   it('should delete file', async () => {
     expect(await engine.files.remove(testFile.id)).to.have.property('id');
   });
-  it('should fail finding the deleted file');
+
+  it('should find NO files by ID', async () => {
+    const res = await engine.files.find({ _uuid: testFile.id });
+    expect(res).to.be.instanceof(Array).with.length(0);
+  });
+
+  it('should FAIL getting file', async () => {
+    try {
+      await engine.files.get(testFile.id);
+      return Promise.reject('Was expectig the file to be deleted');
+    } catch (er) {}
+  });
 });
